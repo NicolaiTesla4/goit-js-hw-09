7a82cc12c1b49f74566f0efd8e8cc6dd2e199c82
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-21 18:35:13 -0500

    add files

--- dist/01-color-switcher.52e3b619.js
+++ dist/01-color-switcher.52e3b619.js
@@ -507,14 +507,14 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
 }
 let intervalId;
-document.getElementById("data-start").addEventListener("click", function() {
+document.getElementById("startButton").addEventListener("click", function() {
     this.disabled = true;
     intervalId = setInterval(function() {
         document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
 });
-document.getElementById("data-stop").addEventListener("click", function() {
-    document.getElementById("Start").disabled = false;
+document.getElementById("stopButton").addEventListener("click", function() {
+    document.getElementById("startButton").disabled = false;
     clearInterval(intervalId);
 });
 
--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ function getRandomHexColor() {
 
 let intervalId;
 
-document.getElementById("data-start").addEventListener("click", function () {
+document.getElementById("startButton").addEventListener("click", function () {
   this.disabled = true;
 
   intervalId = setInterval(function () {
@@ -14,8 +14,8 @@ document.getElementById("data-start").addEventListener("click", function () {
   }, 1000);
 });
 
-document.getElementById("data-stop").addEventListener("click", function () {
-  document.getElementById("Start").disabled = false;
+document.getElementById("stopButton").addEventListener("click", function () {
+  document.getElementById("startButton").disabled = false;
 
   clearInterval(intervalId);
 });
