48e291d4c1e060f1fd80e8c46194c7e01fda92bf
NicolaiTesla4 <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-21 23:35:41 +0000

    Deploying to gh-pages from @ NicolaiTesla4/goit-js-hw-09@7a82cc12c1b49f74566f0efd8e8cc6dd2e199c82 🚀

--- 01-color-switcher.ed063177.js
+++
@@ -1,2 +0,0 @@
-let t;document.getElementById("data-start").addEventListener("click",(function(){this.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),document.getElementById("data-stop").addEventListener("click",(function(){document.getElementById("Start").disabled=!1,clearInterval(t)}));
-//# sourceMappingURL=01-color-switcher.ed063177.js.map
