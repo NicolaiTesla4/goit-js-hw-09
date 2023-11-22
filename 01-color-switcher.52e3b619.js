36d32881d31beeb9e64ebf76a85110ebb6dc1e64
NicolaiTesla4 <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-22 03:20:20 +0000

    Deploying to gh-pages from @ NicolaiTesla4/goit-js-hw-09@43810248fcadf27b65b8996b4bd4cf51bac57d6b 🚀

--- 01-color-switcher.52e3b619.js
+++ 01-color-switcher.52e3b619.js
@@ -503,31 +503,7 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"cYUEh":[function(require,module,exports) {
-/* function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, "0")}`;
-}
-
-let intervalId;
-
-document.getElementById("startButton").addEventListener("click", function () {
-  this.disabled = true;
-
-  intervalId = setInterval(function () {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-});
-
-document.getElementById("stopButton").addEventListener("click", function () {
-  document.getElementById("startButton").disabled = false;
-
-  clearInterval(intervalId);
-});
-
-console.log(st);
-
- */ function getRandomHexColor() {
+function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
 }
 const startButton = document.getElementById("startButton");
