43810248fcadf27b65b8996b4bd4cf51bac57d6b
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-21 22:19:18 -0500

    add files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,3 @@
-/* function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, "0")}`;
-}
-
-let intervalId;
-
-document.getElementById("startButton").addEventListener("click", function () {
-  this.disabled = true;
-
-  intervalId = setInterval(function () {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-});
-
-document.getElementById("stopButton").addEventListener("click", function () {
-  document.getElementById("startButton").disabled = false;
-
-  clearInterval(intervalId);
-});
-
-console.log(st);
-
- */
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
