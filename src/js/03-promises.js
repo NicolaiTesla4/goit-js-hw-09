fa6c889387d6805c9c5de0f1de63c0adae9c5cc2
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2023-11-27 22:13:29 -0500

    add files 6

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,5 @@
 import Notiflix from "notiflix";
 
-/* function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-} */
-
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
